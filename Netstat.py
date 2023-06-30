3f4b618ea8ae8968aca5aaec0a5332b7f660905d
GuilhermeTart <136984328+GuilhermeTart@users.noreply.github.com>
2023-06-30 13:14:31 -0300

    Update Netstat.py

--- Netstat.py
+++ Netstat.py
@@ -12,4 +12,13 @@ execute_netstat()
 
 
 
- 
+def execute_tasklist(pid):
+    try:
+        command = f'tasklist | findstr "{pid}"'
+        output = subprocess.check_output(command, shell=True)
+        print(output.decode('latin-1'))
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao executar o comando tasklist: {e}")
+
+pid = input("Informe seu PID: ")
+execute_tasklist(pid)
