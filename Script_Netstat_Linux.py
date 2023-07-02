8694d80ab6672aca60fb32f19ba469b697c87c9f
GuilhermeTart <136984328+GuilhermeTart@users.noreply.github.com>
2023-07-02 20:46:50 -0300

    Update Script_Netstat_Linux.py

--- Script_Netstat_Linux.py
+++ Script_Netstat_Linux.py
@@ -3,6 +3,23 @@
 import subprocess
 
 
+print(r"""
+  ____ _   _ ___ _     _   _ _____ ____  __  __ _____      _   _   _  ____ _   _ ____ _____ ___  
+ / ___| | | |_ _| |   | | | | ____|  _ \|  \/  | ____|    / \ | | | |/ ___| | | / ___|_   _/ _ \ 
+| |  _| | | || || |   | |_| |  _| | |_) | |\/| |  _|     / _ \| | | | |  _| | | \___ \ | || | | |
+| |_| | |_| || || |___|  _  | |___|  _ <| |  | | |___   / ___ \ |_| | |_| | |_| |___) || || |_| |
+ \____|\___/|___|_____|_| |_|_____|_| \_\_|  |_|_____| /_/   \_\___/ \____|\___/|____/ |_| \___/ 
+                                                                                                 
+             __  __  ___  _   _ ___ _____ ___  ____    _   _ _____ _______        _____  ____  _  __
+            |  \/  |/ _ \| \ | |_ _|_   _/ _ \|  _ \  | \ | | ____|_   _\ \      / / _ \|  _ \| |/ /
+            | |\/| | | | |  \| || |  | || | | | |_) | |  \| |  _|   | |  \ \ /\ / / | | | |_) | ' / 
+            | |  | | |_| | |\  || |  | || |_| |  _ <  | |\  | |___  | |   \ V  V /| |_| |  _ <| . \ 
+            |_|  |_|\___/|_| \_|___| |_| \___/|_| \_\ |_| \_|_____| |_|    \_/\_/  \___/|_| \_\_|\_\
+                                                                                                    
+
+""")
+
+
 #Essa função executa o comando 'netstat ss -tunap'. O parametro 'ss' faz uma anásile de sockets no linux, que pode fornecer informações detalhadas sobre a conexão de rede, e a opção '-tunap' t= Exibe conexãoes TCP, u= Exibe conexões UDP, n= Exibe número de portas ao invés de nomes de serviço, a= Inclui sockets de escuta e conexões não estabelecidas, p= Mostra informações sobre os processos associados a cada conexão. Após isso irá printar a saída do comando com a formatação 'utf-8', ou caso ocorra algum erro, irá exibir uma mensagem de erro.
 def execute_netstat():
     try:
